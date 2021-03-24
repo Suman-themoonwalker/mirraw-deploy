37017bd17bade6e7be9e8a22f436dd897a8bec14
Suman-themoonwalker <sumandsm00@gmail.com>
2021-03-24 17:47:00 +0530

    Cards updated

--- src/App.js
+++ src/App.js
@@ -621,7 +621,88 @@ function App() {
         <a href="#"><img src="https://i.postimg.cc/tJKvsMWT/roots-of-india.jpg" class="roots_of_india" /></a>
         <a href="#"><img src="https://i.postimg.cc/XYwmC420/footwear.jpg" class="footwear" /></a>
         </div>
-      </section>
+</section>
+<section>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/gjXBkWwQ/sarees.jpg" class="m_sarees" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/rw67sKNJ/cover.jpg" class="m_cover" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/SsKv38my/kurta-sets.jpg" class="m_kurta" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/k4M0CzvP/anarkali-suits.jpg" class="m_anarkali"/></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/Y2vpmLQK/lehenga.png" class="m_lehenga" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/26XKD65n/necklace-sets.jpg" class="m_necklace_sets" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/TY5ZTrg1/kidsw-ear.jpg" class="m_kidswear" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/8CN3d93f/mensw-ear.jpg" class="m_menswear" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/WzCCM3Ls/earrings.jpg" class="m_earrings" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/pdCSskbG/home-decor.jpg" class="m_home_decor" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/4ymj7c08/bags.jpg" class="m_bags" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/tJKvsMWT/roots-of-india.jpg" class="m_roots_of_india" /></a>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-block">
+    <a href="#"><img src="https://i.postimg.cc/XYwmC420/footwear.jpg" class="m_footwear" /></a>
+  </div>
+</div>
+
+</section>
 
 
 <section>
